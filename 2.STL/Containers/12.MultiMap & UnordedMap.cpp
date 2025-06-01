665d281be38fc70a72870aa859f81a5158efcb14
prahadeeshwaran-k <prahadeeshwaran.mk@gmail.com>
2025-06-01 18:49:41 +0530

    Containers

---
+++ 2.STL/Containers/12.MultiMap & UnordedMap.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <map>
+using namespace std;
+// * https://www.geeksforgeeks.org/multimap-associative-containers-the-c-standard-template-library-stl/
+// * https://www.geeksforgeeks.org/unordered_set-in-cpp-stl/
+
+// ? MultiMap
+// ! store Duplicate Keys
+// ! stores in SORTED order
+
+// ? UnorderMap
+// ! store UNIQUE Keys
+// ! Stored in a RANDOM order
+
+
+int main(){
+    
+}
