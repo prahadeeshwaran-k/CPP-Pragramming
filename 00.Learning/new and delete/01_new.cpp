2c9537724e4db3481a719d623a5e29bc77ca0176
prahadeeshwaran-k <prahadeeshwaran.mk@gmail.com>
2025-09-16 19:47:10 +0530

    new

--- 00.Learning/new and delete/01_new.cpp
+++ 00.Learning/new and delete/01_new.cpp
@@ -5,9 +5,16 @@ using namespace std;
 int main(){
     int * ptr1 = new int;
     int * ptr2 = new int;
+    
+    if (!ptr1 && !ptr2) {
+        cout << "allocation of memory failed";
+        exit(0);
+    }
 
     *ptr1 = 10;
     *ptr2 = 20;
 
     cout <<*ptr1<<" + "<<*ptr2<<" = "<<*ptr1 + *ptr2 << endl;
+    delete ptr1,ptr2;
+    ptr1 = ptr2 = nullptr;//use to avoid the dangling pointer
 }
